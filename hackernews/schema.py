11e8e4d5e683e0e9def02484ca8b99dcee0f9d4f
Aditya <adityaprakash.bobby@gmail.com>
2019-06-25 00:32:11 +0530

    Mutation for creating users

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -1,12 +1,11 @@
 import graphene
-
 import links.schema
-
+import users.schema
 
 class Query(links.schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(links.schema.Mutation, graphene.ObjectType):
+class Mutation(links.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
