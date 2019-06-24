4bc3bdd0b5406bd72dd0fee882ec50792fd6b49d
Aditya <adityaprakash.bobby@gmail.com>
2019-06-25 00:38:36 +0530

    Query method for listing all users

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -2,7 +2,7 @@ import graphene
 import links.schema
 import users.schema
 
-class Query(links.schema.Query, graphene.ObjectType):
+class Query(links.schema.Query, users.schema.Query, graphene.ObjectType):
     pass
 
 class Mutation(links.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
