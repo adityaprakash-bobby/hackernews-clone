5eb759bffdd1652630210c7e005027ced7a253cd
Aditya <adityaprakash.bobby@gmail.com>
2019-06-30 13:53:07 +0530

    add resolver to show current user; added authetication tokens

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -1,4 +1,5 @@
 import graphene
+import graphql_jwt
 import links.schema
 import users.schema
 
@@ -6,6 +7,9 @@ class Query(links.schema.Query, users.schema.Query, graphene.ObjectType):
     pass
 
 class Mutation(links.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
-    pass
+    
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
